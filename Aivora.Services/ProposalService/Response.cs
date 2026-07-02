58f3e1b2d44ffdfaae6493e16f6d77180d1624eb
tranhquan099-commits <tranhquan099@users.noreply.github.com>
2026-07-02 16:29:35 +0700

    feat(proposal): add client info to proposal response

--- Aivora.Services/ProposalService/Response.cs
+++ Aivora.Services/ProposalService/Response.cs
@@ -9,6 +9,8 @@ public class Response
         public Guid Id { get; set; }
         public Guid JobId { get; set; }
         public string JobTitle { get; set; } = null!;
+        public Guid ClientId { get; set; }
+        public string ClientName { get; set; } = null!;
         public Guid ExpertId { get; set; }
         public string ExpertName { get; set; } = null!;
         public string CoverLetter { get; set; } = null!;
